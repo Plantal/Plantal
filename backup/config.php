c3de884b28b4d4377264fb587d67c6f8330b5fe4
Samanaras <alvarocosta5555@gmail.com>
2019-04-26 11:52:20 +0100

    testebackup 4

---
+++ backup/config.php
@@ -0,0 +1,15 @@
+<?php
+
+return array(
+    'backup_dir'    => '/var/www/html/backup',
+    'keep_files'    => 50,
+    'compression'   => '', 
+    'db_host'       => 'localhost',
+    'db_port'       => '3306',
+    'db_protocol'   => 'tcp',
+    'db_user'       => 'root',
+    'db_passwd'     => 'root',
+    'db_names'      => array(
+        'plantal' 
+    )
+);
