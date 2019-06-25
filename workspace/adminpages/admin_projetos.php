917815fc4123804c5587a9ec8735f1411a7f1538
Samanaras <alvarocosta5555@gmail.com>
2019-06-25 06:15:18 +0100

    projeto2

--- workspace/adminpages/admin_projetos.php
+++ workspace/adminpages/admin_projetos.php
@@ -230,6 +230,13 @@ if(!isset($_SESSION['ativa'])){
             <div id = "mapid"></div>
       <script>
         var mymap = L.map('mapid').setView([51.505, -0.09], 13); 
+
+        L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox.streets',
+    accessToken: 'your.mapbox.access.token'
+}).addTo(mymap);
       </script>
  
         
