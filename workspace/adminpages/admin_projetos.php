968d7d268856367e24fea0f8991b0e059d83a329
Samanaras <alvarocosta5555@gmail.com>
2019-06-25 06:26:05 +0100

    projeto2

--- workspace/adminpages/admin_projetos.php
+++ workspace/adminpages/admin_projetos.php
@@ -231,11 +231,11 @@ if(!isset($_SESSION['ativa'])){
       <script>
         var mymap = L.map('mapid').setView([51.505, -0.09], 13); 
 
-        L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={pk.eyJ1Ijoic2FtYW5hcmFzIiwiYSI6ImNqeGJkMGJxcTBhOGMzenBuamk1d2hwNnIifQ.UQ6ZZj66vFkkXSdURMapXw}', {
+        L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     id: 'mapbox.streets',
-    accessToken: 'pk.eyJ1Ijoic2FtYW5hcmFzIiwiYSI6ImNqeGJkMGJxcTBhOGMzenBuamk1d2hwNnIifQ.UQ6ZZj66vFkkXSdURMapXw'
+    accessToken: 'pk.eyJ1Ijoic2FtYW5hcmFzIiwiYSI6ImNqeGJkOWFvazBkaHUzb21iN3BpdWFicncifQ.X1hC8wt85s_Uok8aaQzKSw'
 }).addTo(mymap);
       </script>
  
