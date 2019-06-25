da77904fe595c241ac1ab3c7afff0a56d0aa4d4a
Samanaras <alvarocosta5555@gmail.com>
2019-06-25 06:19:20 +0100

    projeto2

--- workspace/adminpages/admin_projetos.php
+++ workspace/adminpages/admin_projetos.php
@@ -231,7 +231,7 @@ if(!isset($_SESSION['ativa'])){
       <script>
         var mymap = L.map('mapid').setView([51.505, -0.09], 13); 
 
-        L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+        L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={pk.eyJ1Ijoic2FtYW5hcmFzIiwiYSI6ImNqeGJkMGJxcTBhOGMzenBuamk1d2hwNnIifQ.UQ6ZZj66vFkkXSdURMapXw}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     id: 'mapbox.streets',
