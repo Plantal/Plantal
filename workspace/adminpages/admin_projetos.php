fb580835b40e9150f63254e54cf3f9d6c4457ca9
Samanaras <alvarocosta5555@gmail.com>
2019-06-25 09:43:14 +0100

    projeto3

--- workspace/adminpages/admin_projetos.php
+++ workspace/adminpages/admin_projetos.php
@@ -262,7 +262,7 @@ if(!isset($_SESSION['ativa'])){
 
       
     <script async defer
-    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBx67Sngc8Ij0vkQDl2Uy9Ffwc6Eb_GPxo&callback=initMap">
+    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBx67Sngc8Ij0vkQDl2Uy9Ffwc6Eb_GPxo&callback=loadMap">
     </script>
  
 
