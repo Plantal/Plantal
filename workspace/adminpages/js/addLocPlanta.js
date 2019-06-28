16f99614d8cbda899378b4755ed33c6e00298521
Samanaras <alvarocosta5555@gmail.com>
2019-06-28 07:38:59 +0100

    mapaProjeto

--- workspace/adminpages/js/addLocPlanta.js
+++ workspace/adminpages/js/addLocPlanta.js
@@ -1,24 +1,15 @@
-var map;
-var geocoder;
-
-
-
-function loadMap() {
-	var viana = {lat: 41.6946, lng: -8.83016};
-    map = new google.maps.Map(document.getElementById('mapid'), {
-      zoom: 13,
-      center: viana
-    });
-
-}
-
 google.maps.event.addListener(map, 'click', function(event) {
-   placeMarker(event.latLng);
+  placeMarker(map, event.latLng);
 });
 
-function placeMarker(location) {
-    var marker = new google.maps.Marker({
-        position: location, 
-        map: map
-    });
+function placeMarker(map, location) {
+  var marker = new google.maps.Marker({
+    position: location,
+    map: map
+  });
+  var infowindow = new google.maps.InfoWindow({
+    content: 'Latitude: ' + location.lat() +
+    '<br>Longitude: ' + location.lng()
+  });
+  infowindow.open(map,marker);
 }
