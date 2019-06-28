65ad3147ace93180488902dd6bc41b791c453803
Samanaras <alvarocosta5555@gmail.com>
2019-06-28 15:34:29 +0100

    tabela

--- workspace/adminpages/admin_projetos.php
+++ workspace/adminpages/admin_projetos.php
@@ -246,6 +246,8 @@ if(!isset($_SESSION['ativa'])){
 
 <?php  
 $result = mysqli_query($connect,"SELECT idProjeto, nome, username FROM projeto, users WHERE projeto.userId = users.iduser ");
+ var_dump($result);
+ 
 
 while($row = mysqli_fetch_array($result))
 {
