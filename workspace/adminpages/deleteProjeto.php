36f388e8811fc2654be3eb4d86353a29514a3357
Samanaras <alvarocosta5555@gmail.com>
2019-07-01 04:02:56 +0100

    editModals

--- workspace/adminpages/deleteProjeto.php
+++ workspace/adminpages/deleteProjeto.php
@@ -10,12 +10,14 @@ session_start();
 
 
  	$query = "DELETE FROM projeto_plantal WHERE projetoId = '".$_POST["idProjeto"]."'"; 
-
+echo $query;
  	if(mysqli_query($connect, $query); ){
 
          
       $query2 = "DELETE FROM projeto WHERE idProjeto = '".$_POST["idProjeto"]."'";  
       mysqli_query($connect, $query2);  
+
+      echo $query2;
       }
  }  
  ?>
