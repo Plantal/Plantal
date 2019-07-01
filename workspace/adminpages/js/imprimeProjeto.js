cd22f1359409a804060c0991924a8f898446a72e
Samanaras <alvarocosta5555@gmail.com>
2019-07-01 07:07:54 +0100

    vProj

---
+++ workspace/adminpages/js/imprimeProjeto.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+
+$("#imprimir").click( function(){
+            $('.btn').each(function() {
+              var container = $(this);
+              idProjeto = container.data('service');
+
+        console.log(idProjeto);
+
+        imprimir(idProjeto);
+    });
+
+
+          });
+
+
+          function imprimir(id) {
+            window.open("http://flora.ipvc.pt/workspace/adminpages/pdfProjeto.php?idProjeto="+id);
+
+          }
+	
+
+});
+
+
+
--- workspace/adminpages/viewProjetos.php
+++ workspace/adminpages/viewProjetos.php
@@ -307,7 +307,7 @@ while($row = mysqli_fetch_array($result))
                   </tbody>
                 </table>
               </div>
-<input type="button" name="create_pdf" class="btn btn-danger" value="Imprimir QrCodes"  onclick="window.open('http://flora.ipvc.pt/workspace/adminpages/pdfProjeto.php?idProjeto='$_GET['idProjeto']')">  </input>
+                <input type="button" id="imprimir" name="create_pdf" class="btn btn-danger"  data-service="<?php echo $row["idProjeto"]; ?>" value="Imprimir QrCodes"  onclick="window.open('http://flora.ipvc.pt/workspace/adminpages/pdfProjeto.php?idProjeto='')">  </input>
 
                    
                             
