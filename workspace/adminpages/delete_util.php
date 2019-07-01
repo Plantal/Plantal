81d496ba5fd7dc4e3b053735be791a630867c65d
Samanaras <alvarocosta5555@gmail.com>
2019-07-01 01:06:26 +0100

    usersAdmin

--- workspace/adminpages/delete_util.php
+++ workspace/adminpages/delete_util.php
@@ -5,10 +5,16 @@ session_start();
      header('Location: login.php');
    
    }
+
+
  if(isset($_POST["iduser"]))  
  {  
-         
+         echo $_POST["iduser"];
+
+
       $query = "DELETE FROM users WHERE iduser = '".$_POST["iduser"]."'";  
+
+      echo $query;
       $result = mysqli_query($connect, $query);  
       
  }  
