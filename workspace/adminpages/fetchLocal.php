9208fdd8f67a3d13094d2b2e06000b4ad5aca057
Samanaras <alvarocosta5555@gmail.com>
2019-07-01 09:04:30 +0100

    vProj

--- workspace/adminpages/fetchLocal.php
+++ workspace/adminpages/fetchLocal.php
@@ -6,9 +6,12 @@ session_start();
      header('Location: login.php');
    
    }
+
+echo $_POST['id'];
+
  if(isset($_POST["id"]))  
  {  
-      $query = "SELECT  latitude, longitude, nomeCientifico FROM projeto_plantal, geolocal WHERE projeto_plantal.geocodeId = geolocal.idGeolocal AND  id = '".$_POST["id"]."'";  
+      $query = "SELECT geolocal.latitude, geolocal.longitude, nomeCientifico FROM projeto_plantal, geolocal, planta WHERE projeto_plantal.geocodeId = geolocal.idGeolocal AND projeto_plantal.plantaId = planta.idPlanta AND id = '".$_POST["id"]."' ";  
       $result = mysqli_query($connect, $query);  
       $row = mysqli_fetch_array($result);  
       echo json_encode($row);  
