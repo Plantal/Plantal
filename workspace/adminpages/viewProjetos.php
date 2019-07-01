f1dd0cafa369653f3bd2e7d0745cfb3f01d44a23
Samanaras <alvarocosta5555@gmail.com>
2019-07-01 06:50:21 +0100

    vProj

--- workspace/adminpages/viewProjetos.php
+++ workspace/adminpages/viewProjetos.php
@@ -308,7 +308,7 @@ while($row = mysqli_fetch_array($result))
                 </table>
               </div>
               
-                     <input type="button" name="create_pdf" class="btn btn-danger" value="Imprimir QrCodes"  onclick="window.open('http://flora.ipvc.pt/workspace/adminpages/pdfProjeto.php')">  </input>
+                     <input type="button" name="create_pdf" class="btn btn-danger" value="Imprimir QrCodes"  onclick="window.open('http://flora.ipvc.pt/workspace/adminpages/pdfProjeto.php?idProjeto='.$_GET['idProjeto'].')">  </input>
 
                    
                             
