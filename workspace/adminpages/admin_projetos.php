2d023341a7e27e0059d7885dc8571423929f6601
Samanaras <alvarocosta5555@gmail.com>
2019-07-01 02:57:51 +0100

    editPlanta

--- workspace/adminpages/admin_projetos.php
+++ workspace/adminpages/admin_projetos.php
@@ -263,7 +263,7 @@ while($row = mysqli_fetch_array($result))
 
       
       
-      <a href="#editProjeto" name="edit" class="edit_data" id="<?php echo $row["idProjeto"]; ?>" data-toggle="modal"><i class="material-icons" data-toggle="tooltip" title="Edit" style="color:yellow;">&#xE254;</i></a>
+      <a href="#tone" name="edit" class="edit_data" id="<?php echo $row["idProjeto"]; ?>" data-toggle="modal"><i class="material-icons" data-toggle="tooltip" title="Edit" style="color:yellow;">&#xE254;</i></a>
 
       <a href="#myModal" name="delete" class="delete_data" data-toggle="modal" id="<?php echo $row["idProjeto"]; ?>"><i class="material-icons" data-toggle="tooltip" title="Delete" style="color: red;">&#xE872;</i></a>
      
@@ -284,6 +284,25 @@ while($row = mysqli_fetch_array($result))
 </div>
 
 
+<div class="modal" id="tone" tabindex="-1" role="dialog">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title">Modal title</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <p>Modal body text goes here.</p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
 
 
 <!-- Modal para editar projetos -->
